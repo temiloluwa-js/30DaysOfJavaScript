16a174cf04009a2ceb2c41c7e66ce6928da155fc
Adeleye-Temiloluwa <temiloluwagithub@gmail.com>
2022-03-27 12:33:53 +0100

    JavaScript

--- Day3.js
+++ Day3.js
@@ -2,3 +2,9 @@ let a  = prompt("Enter the length of the triangle: ")
 let b = prompt("Enter the breadth of the triangle")
 
 alert(`The area of the triangle is ${(a * b) / 2}`)
+
+let sideA  = prompt("Enter the first side of the triangle: ")
+let sideB = prompt("Enter the second side of the triangle")
+let sideC = prompt("Enter the third side of the trinagle")
+
+alert(`The perimeter of the triangle is ${sideA + sideB + sideC}`)
