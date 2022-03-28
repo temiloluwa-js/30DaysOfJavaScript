eba1312523d96c51dc79c999209141889e1f9265
Adeleye-Temiloluwa <temiloluwagithub@gmail.com>
2022-03-28 10:25:51 +0100

    Day 4

--- Day3.js
+++ Day3.js
@@ -1,21 +1,24 @@
-let a  = prompt("Enter the length of the triangle: ")
-let b = prompt("Enter the breadth of the triangle")
+// let a  = prompt("Enter the length of the triangle: ")
+// let b = prompt("Enter the breadth of the triangle")
 
-alert(`The area of the triangle is ${(a * b) / 2}`)
+// alert(`The area of the triangle is ${(a * b) / 2}`)
 
-let sideA  = prompt("Enter the first side of the triangle: ")
-let sideB = prompt("Enter the second side of the triangle")
-let sideC = prompt("Enter the third side of the trinagle")
+// let sideA  = prompt("Enter the first side of the triangle: ")
+// let sideB = prompt("Enter the second side of the triangle")
+// let sideC = prompt("Enter the third side of the trinagle")
 
-alert(`The perimeter of the triangle is ${sideA + sideB + sideC}`)
+// alert(`The perimeter of the triangle is ${sideA + sideB + sideC}`)
 
-let userAge = prompt("How old are you?")
-if (userAge >= 18){
-    alert("allowed to drive!")
-}
+// let userAge = prompt("How old are you?")
+// if (userAge >= 18){
+//     alert("allowed to drive!")
+// }
 
-let noOfYears = prompt("How many years you live?")
-alert(noOfYears * 365 * 24 * 3600)
+// let noOfYears = prompt("How many years you live?")
+// alert(noOfYears * 365 * 24 * 3600)
 
-let date = new Date()
-alert(`${date.getFullYear}  `)
+// let date = new Date()
+// alert(`${date.getFullYear}  `)
+let text = "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python"
+let texta = Array.from(text.split(' '))
+console.log(texta)
