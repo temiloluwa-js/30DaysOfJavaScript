50b71d06adf25208e4b811966794c56b1a5db967
Adeleye-Temiloluwa <temiloluwagithub@gmail.com>
2022-03-29 10:40:49 +0100

    Objects

--- Day3.js
+++ Day3.js
@@ -1,27 +1,21 @@
-function shuffleArray(array) {
-  for (i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * i + 1);
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+const person = {
+  firstName: 'Adeleye',
+  lastName: 'Temiloluwa',
+  age: 17,
+  country: 'NIgeria',
+  city: 'Abeokuta',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Python',
+    'Django',
+    'MySQL'
+  ],
+  getFullName: function(){
+    return `${this.firstName} ${this.lastName}`
   }
-  return array;
 }
 
-console.log(shuffleArray([1, 2, 3, 4, 7]));
-
-function isPrime(number) {
-  let isPrime = true;
-  for (i = 2; i < number; i++) {
-    if (number % i == 0) {
-      isPrime = false;
-      break;
-    }
-  }
-  return isPrime;
-}
-
-console.log(isPrime(23));
-
-
-
+let uncle = Object.assign({}, person)
+console.log(Object.values(uncle))
